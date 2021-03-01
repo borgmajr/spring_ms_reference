13a05abd2a67355d3f36dabff72d7a658a90c33c
Jon Borgman <jon.borgman@lnssi.com>
2021-03-01 12:38:13 -0500

    Add files via upload
    INIT

---
+++ src/main/java/com/lnssi/microserviceX/ReferenceResTfulMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.lnssi.microserviceX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceResTfulMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferenceResTfulMicroserviceApplication.class, args);
+	}
+
+}
